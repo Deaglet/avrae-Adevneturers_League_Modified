3094bb3807ef6c1e027900db08fbcc92a80608b8
rekaj3773 <rekaj3773@gmail.com>
2019-08-14 22:22:55 -0400

    Update charGen.py
    Forgot return

--- cogs5e/charGen.py
+++ cogs5e/charGen.py
@@ -78,7 +78,7 @@ class CharGenerator(commands.Cog):
     @commands.command(aliases=['rekaj_name'])
     async def randname(self, ctx):
         """Generates a random name, optionally from a given race."""
-        await ctx.send(f"Your random 3-5 char rekaj name: {self.rekaj_name_gen()}")
+        return await ctx.send(f"Your random 3-5 char rekaj name: {self.rekaj_name_gen()}")
 
     @commands.command(name='charref', aliases=['makechar'])
     async def char(self, ctx, level):
