f0d5aedce5e0e8fe68d61e9a6952cded6ca1bbec
rekaj3773 <rekaj3773@gmail.com>
2019-12-26 13:50:38 -0500

    new shell for running global bot
    adding global sh to restart main bot if needed

--- keepAvraeBotRunning.sh
+++ keepAvraeBotRunning.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Scripts to start services if not running
 
-ps -ef | grep dbot |grep -v grep > /dev/null
+ps -ef | grep rekaj_dbot |grep -v grep > /dev/null
 if [ $? != 0 ]
 then
-       python3 dbot.py test 
+       sudo python3 rekaj_dbot.py test start > /dev/null 
 fi
