3705f07ab31baebf7cbfd4831aaf57961254a26e
DAvery <dustin.avery>
2020-01-17 15:24:30 -0600

    Working on edge system

--- cogsmisc/shadowrun.py
+++ cogsmisc/shadowrun.py
@@ -60,6 +60,66 @@ class Shadowrun(commands.Cog):
                 output_str += "*GLITCH*"
         await ctx.send(output_str)
 
+    @commands.command(name='edge', aliases=['e'])
+    async def edge(self, ctx, runner_name):
+        characterDocuments = self.bot.mdb.character.find({"runner_name":runner_name}).limit(1)
+        await ctx.send(characterDocuments[0]["edge"])
+
+    @commands.command(name='addRunner', aliases=['ar','add_runner'])
+    async def addRunner(self, ctx, runner_name, edgeCount=0):
+        if self.doesRunnerExist(runner_name):
+            await ctx.send("Runner " + runner_name + " already exists")
+        else:
+            await self.bot.mdb.character.insert_one({"runner_name":runner_name, "edge_stat":edgeCount, "edge": edgeCount})
+            await ctx.send("Runner Added")
+
+    @commands.command(name='setEdge', aliases=['sete','setedge'])
+    async def setEdge(self, ctx, runner_name, edge):
+        if self.doesRunnerExist(runner_name):
+            self.bot.mdb.character.update_one({"runner_name":runner_name}, {"$set":{"edge":edge}}, upsert=True)
+        else:
+            self.addRunner(runner_name, edge)
+
+        await ctx.send("Runner edge has been set")
+
+    @commands.command(name='addEdge', aliases=['ae','aEdge','aedge'])
+    async def addEdge(self, ctx, runner_name, edge):
+        self.changeEdge(ctx, runner_name, edge, True)
+
+    @commands.command(name='subtractEdge', aliases=['se','sEdge','sedge'])
+    async def addEdge(self, ctx, runner_name, edge):
+        self.changeEdge(ctx, runner_name, edge, True)
+
+    # Todo: this method probably does too much
+    def changeEdge(self, ctx, runner_name, edge, positive):
+        str = ""
+        if self.doesRunnerExist(runner_name):
+            oldEdge = self.bot.mdb.character.find({"runner_name":runner_name}).limit(1)[0]["edge"]
+            if positive:
+                newEdge = oldEdge + edge
+            else:
+                newEdge = oldEdge + edge
+            if newEdge > 7:
+                newEdge = 7
+                str += "Max edge of 7 reached, setting to 7. \n"
+            elif newEdge <= 0:
+                newEdge = 0
+            self.bot.mdb.character.update_one({"runner_name":runner_name}, {"$set":{"edge":newEdge}}, upsert=True)
+        else:
+            await ctx.send("Runner does not exist, adding runner")
+            self.addRunner(runner_name, edge)
+
+        if positive:
+            str += "Edge has been added, Total Edge: " + newEdge
+        else:
+            str += "Edge has been removed, Total Edge: " + newEdge
+        await ctx.send(str)
+
+    def doesRunnerExist(self, runner_name):
+        characterDocument = self.bot.mdb.character.find({"runner_name":runner_name})
+        if len(characterDocument) == 0:
+            return False
+        return True
 
 def setup(bot):
     bot.add_cog(Shadowrun(bot))
