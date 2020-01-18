5dbda1087fecef91e41b56c9c3bf532e9229da35
Dustin-PC\Dustin <1998davery2013@gmail.com>
2020-01-17 20:48:33 -0600

    :(

--- cogsmisc/shadowrun.py
+++ cogsmisc/shadowrun.py
@@ -64,7 +64,8 @@ class Shadowrun(commands.Cog):
     async def edge(self, ctx, runner_name):
         characterDocuments = self.bot.mdb.shadowrunners.find({"runner_name":runner_name}).limit(1)
         charList = characterDocuments.to_list(1)
-        await ctx.send("Am I crazy"+ str(charList[0]["edge"]))
+        edge = await charList[0]["edge"]
+        await ctx.send("Am I crazy"+ str(edge))
 
     @commands.command(name='addRunner', aliases=['ar','add_runner'])
     async def addRunner(self, ctx, runner_name, edgeCount=0):
