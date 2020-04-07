9a30a6dc04e2455613e7548b27945695ff8a5665
Jacob Farneth <rekaj3773@gmail.com>
2020-04-07 15:25:45 -0400

    Update thankYouNitro.py
    Removed function since not needed. look like Graveyard prior example

--- cogsmisc/nitro/thankYouNitro.py
+++ cogsmisc/nitro/thankYouNitro.py
@@ -12,19 +12,16 @@ class ThankYou(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['Homegrown'])
-    async def condition(self, ctx, *, name: str):
-
+    @commands.command(name='Homegrown',aliases=['Homegrown'])
         
-        colorEmbed = random.randint(0,3822)
-        embed = discord.Embed()
-        embed.colour = colorEmbed
-        #embed.title = self.get_name()
-        embed.title = "Homegrown"
-        embed.add_field(value=f"`Aw shucks, Hux.  Being against your axe, sucks!`")
-        #embed.set_footer(*, text="Thank you for being a [Nitro Supporter](https://discordapp.com/nitro)", icon_url="cogsmisc\nitro\nitro_icon.svg")
-        embed.set_footer(text=f"Thank you for being a [Nitro Supporter](https://discordapp.com/nitro)")
-        return embed
+    colorEmbed = random.randint(0,3822)
+    embed = discord.Embed()
+    embed.colour = colorEmbed
+    embed.title = "Homegrown"
+    embed.add_field(value=f"`Aw shucks, Hux.  Being against your axe, sucks!`")
+    #embed.set_footer(*, text="Thank you for being a [Nitro Supporter](https://discordapp.com/nitro)", icon_url="cogsmisc\nitro\nitro_icon.svg")
+    embed.set_footer(text=f"Thank you for being a [Nitro Supporter](https://discordapp.com/nitro)")
+    return embed
 
 
 def setup(bot):
