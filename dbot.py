c0414d4b14cfcc7283971b1f31e54d241a18f90e
Dustin Avery <1998davery2013@gmail.com>
2020-07-22 12:49:52 -0500

    Reverting changes to see if it fixes it

--- dbot.py
+++ dbot.py
@@ -121,7 +121,7 @@ msglog = logging.getLogger('messages')
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="your nat 1s"))
+    # await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="your nat 1s"))
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
